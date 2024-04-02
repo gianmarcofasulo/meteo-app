576ea7023094c40373012ec710ada8a392b35372
Gianmarco <gianmarcofasulo@gmail.com>
2024-04-02 16:48:13 +0200

    Location

---
+++ AddCityPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MeteoApp;
+
+public partial class AddCityPage : ContentPage
+{
+	public AddCityPage()
+	{
+		InitializeComponent();
+	}
+}
--- Services/ApiService.cs
+++ Services/ApiService.cs
@@ -8,6 +8,7 @@ namespace MeteoApp.Services
         public static async Task<Root> GetWeatherData(double latitude, double longitude)
         {
             var httpClient = new HttpClient();
+            httpClient.Timeout = TimeSpan.FromSeconds(200);
             var response = await httpClient.GetStringAsync(string.Format("https://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&units=metric&appid=36b65f683451847f0b60287f7d7c2817", latitude, longitude));     
             return JsonConvert.DeserializeObject<Root>(response);    
         }
