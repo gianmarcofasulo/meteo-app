2791ef94299f5aaa980f64593cceb3833c184369
Gianmarco <gianmarcofasulo@gmail.com>
2024-04-02 15:58:55 +0200

    add some feature

--- MeteoListPage.xaml.cs
+++ MeteoListPage.xaml.cs
@@ -1,8 +1,11 @@
-﻿namespace MeteoApp;
+﻿using MeteoApp.Services;
+
+namespace MeteoApp;
 
 public partial class MeteoListPage : Shell
 {
     public Dictionary<string, Type> Routes { get; private set; } = new Dictionary<string, Type>();
+    public List<Models.List> WeatherList;
 
     public MeteoListPage()
 	{
@@ -10,7 +13,29 @@ public partial class MeteoListPage : Shell
         RegisterRoutes();
 
         BindingContext = new MeteoListViewModel();
+        WeatherList = new List<Models.List>();
     }
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        var result = await ApiService.GetWeatherData(45.820599, 8.825058);
+        
+        foreach (var item in result.list)
+        {
+            WeatherList.Add(item);
+        }
+        CvWeather.ItemsSource = WeatherList;
+        
+        LblCity.Text = result.city.name;
+        LblWheatherDesc.Text = result.list[0].weather[0].description;
+        LblTemperature.Text  = result.list[0].main.temperature + "°";
+        LblHumidity.Text  = result.list[0].main.humidity + "%";
+        LblWind.Text  = result.list[0].wind.speed + "km/h";
+        ImgWeatherIcon.Source  = result.list[0].weather[0].customIcon;
+    }
+
+    public async getLocation
 
     private void RegisterRoutes()
     {
---
+++ Services/ApiService.cs
@@ -0,0 +1,22 @@
+﻿using MeteoApp.Models;
+using Newtonsoft.Json;
+
+namespace MeteoApp.Services
+{
+    public static class ApiService
+    {
+        public static async Task<Root> GetWeatherData(double latitude, double longitude)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(string.Format("https://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&units=metric&appid=36b65f683451847f0b60287f7d7c2817", latitude, longitude));     
+            return JsonConvert.DeserializeObject<Root>(response);    
+        }
+
+        public static async Task<Root> GetWeatherByCity(string name)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(string.Format("https://api.openweathermap.org/data/2.5/forecast?q={0}&units=metric&appid=36b65f683451847f0b60287f7d7c2817", name));
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+    }
+}
